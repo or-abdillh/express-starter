236eee96095766b4a219ec594b86e7256f8a6db4
Oka R. Abdillah <hans.abdillh05@gmail.com>
2022-05-21 20:21:49 +0800

    Create route for get all articles

--- app/router/index.js
+++ app/router/index.js
@@ -2,9 +2,12 @@
 
 // Import controllers
 const user = require('../controllers/userController.js')
+const article = require('../controllers/articleController.js')
 
 module.exports = app => {
 	// Register and Login
 	app.route('/register').post( user.register )
 	app.route('/login').post( user.login )
+
+	app.route('/articles').get( article.index )
 }
