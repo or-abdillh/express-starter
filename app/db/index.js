1d614256ea5bec88b61b6063a84a8048a366a40c
Oka R. Abdillah <hans.abdillh05@gmail.com>
2022-05-25 20:19:11 +0800

    Disable logging

--- app/db/index.js
+++ app/db/index.js
@@ -8,7 +8,8 @@ const { Sequelize, DataTypes } = require('sequelize')
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
 	host: process.env.DB_HOST || 'localhost',
 	port: process.env.DB_PORT,
-	dialect: 'postgres'
+	dialect: 'postgres',
+	logging: false
 })
 
 // Define models
