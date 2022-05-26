3ea059fce3b371db10bf3f6d7e3c72d74daf4248
Oka R. Abdillah <hans.abdillh05@gmail.com>
2022-05-26 21:51:48 +0800

    Update connection

--- app/db/index.js
+++ app/db/index.js
@@ -5,14 +5,11 @@ require('dotenv').config({ path: `${process.cwd()}/.env` })
 const { Sequelize, DataTypes } = require('sequelize')
 
 // Define connection
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-	host: process.env.DB_HOST || 'localhost',
-	port: process.env.DB_PORT,
-	dialect: 'postgres',
-	logging: false
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+	dialect: 'postgres'
 })
 
-// Define models
+// Define model
 const models = [
 	require('./models/user.js'),
 	require('./models/article.js')
